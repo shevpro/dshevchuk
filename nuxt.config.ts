4597de671faa8fb8d64134c65a8953ec1a2f7d28
Dmitry Shevchuk <shevchuk.pro@yandex.ru>
2024-11-11 20:57:05 +0200

    update meta config + add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,11 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'Фронтенд-разработчик с более 10 лет опыта'
+      viewport: 'width=device-width, initial-scale=1, user-scalable=no',
+      title: 'Фронтенд-разработчик с более 10 лет опыта',
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;0,900;1,700&display=swap' }
+      ],
     }
   },
 
